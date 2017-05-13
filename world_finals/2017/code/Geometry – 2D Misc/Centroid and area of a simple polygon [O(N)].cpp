ff62ff8cfa7d7059c2a04b50365449f64dbdc715
Franklin Tang <issaday@gmail.com>
2017-05-12 23:30:05 -0600

    Create Centroid and area of a simple polygon [O(N)].cpp

---
+++ world_finals/2017/code/Geometry – 2D Misc/Centroid and area of a simple polygon [O(N)].cpp
@@ -0,0 +1,13 @@
+// Points must be oriented (CW or CCW), and non­convex is OK~
+// Returns (nan,nan) is area of polygon is zero
+point centroid(vector<point> p) {
+    int n = p.size(); // should be at least 1
+    double area = 0; point c(0,0);
+    for(int i = n­1, j = 0; j < n; i = j++) {
+        double a = (conj(p[i])*p[j]).imag()/2; //cross
+        area += a;
+        c += (p[i]+p[j])*(a/3);
+    }
+    c /= area;
+    return c; // or return 'area' for the area of polygon
+}
