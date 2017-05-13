ad454bd94439d91f7b8d10f62ba16050939cf0d5
Franklin Tang <issaday@gmail.com>
2017-05-12 23:29:44 -0600

    Create Rectangle in rectangle test.cpp

---
+++ world_finals/2017/code/Geometry – 2D Misc/Rectangle in rectangle test.cpp
@@ -0,0 +1,13 @@
+// Can rectangle with dims x*y fit inside box with dims w*h?~
+// Returns true for a "tight fit", if false is desired then swap
+// strictness of inequalities.
+bool rect_in_rect(double x, double y, double w, double h) {
+    if (x > y) swap(x, y);
+    if (w > h) swap(w, h);
+    if (w < x) return false;
+    if (y <= h) return true;
+    double a = y*y ­ x*x;
+    double b = x*h ­ y*w;
+    double c = x*w ­ y*h;
+    return a*a <= b*b + c*c;
+}
