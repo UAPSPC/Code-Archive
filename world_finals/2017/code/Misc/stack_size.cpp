e9eb393b23a2332f9677185e0f91380ee5cdbc9c
Noah Weninger <nweninge@ualberta.ca>
2017-05-14 20:14:13 -0400

    Stack size trick

---
+++ world_finals/2017/code/Misc/stack_size.cpp
@@ -0,0 +1,13 @@
+/* Sometimes you want to recurse very deeply or
+   allocate massive arrays on the stack. This code removes the limit
+   on maximum stack size so that you can.
+   Disclaimer:  This is usually not a good idea. You can solve any
+   problem without increasing the max stack size. However, doing so
+   can save you some time in a few cases. */
+#include <sys/resource.h>
+void remove_stack_limit() {
+    struct rlimit rl;
+    getrlimit(RLIMIT_STACK, &rl);
+    rl.rlim_cur = RLIM_INFINITY;
+    setrlimit(RLIMIT_STACK, &rl);
+}
